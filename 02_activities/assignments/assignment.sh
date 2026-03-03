b69da0962c0d5a01c267cc96b3329be672c3c0ff
Michelle Wang <michelle.wang1@sickkids.ca>
2026-03-03 16:41:42 -0500

    Complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,7 +45,7 @@ cd processed
 mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/*server*.log ./data/processed/server_logs
+find ./data/raw -type f -name "*server*.log" -exec cp {} ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/*user*.log ./data/processed/user_logs
